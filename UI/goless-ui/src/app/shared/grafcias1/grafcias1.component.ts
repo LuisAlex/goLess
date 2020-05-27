9400d42950f3afc4cfc2ee34d167803aa7ae17c5
Java do <alex@javado.com>
2020-05-26 21:27:07 -0500

    ui project structure

---
+++ UI/goless-ui/src/app/shared/grafcias1/grafcias1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafcias1',
+  templateUrl: './grafcias1.component.html',
+  styles: [
+  ]
+})
+export class Grafcias1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
